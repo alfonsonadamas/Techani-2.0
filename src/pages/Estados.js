c3a99b61fa2e9926f5eaff41749fb2f1c2c2ea20
Vincitorecito <l20200338@morelia.tecnm.mx>
2024-02-10 23:26:59 -0600

    estado de animo acabado tambien

--- src/pages/Estados.js
+++ src/pages/Estados.js
@@ -29,15 +29,15 @@ export default function Estados() {
     <div>
       <SideBar />
       <div className="p-16 pt-20 sm:ml-64" data-aos="fade-up">
-      <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
-          Registro del estado de ánimo
-        </label>
-          <form onSubmit={handleSubmit} className="max-w-md w-full bg-white p-8 rounded-lg shadow-md mt-8 md:mt-16">
+      <label htmlFor="day" className="block mb-1">Registro del estado de ánimo:</label>
+      
+          <form onSubmit={handleSubmit}>
             
             <div className="mb-4">
-              <label className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" htmlFor="estados">
-                Estado de ánimo:
-              </label>
+            <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+            Estado de ánimo:
+            </label>
+              
               <input
                 className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                 id="estados"
@@ -48,11 +48,14 @@ export default function Estados() {
               />
             </div>
             <div className="mb-4">
-              <label className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">Seleccionar estado de ánimo:</label>
+            <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+            Seleccionar estado de ánimo:
+            </label>
+              {/* <label className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">Seleccionar estado de ánimo:</label> */}
               <div className="flex items-center justify-between">
-              <button type="button" className="text-3xl" onClick={() => handleMoodChange({ target: { value: '😊' } })}>😊</button>
-              <button type="button" className="text-3xl" onClick={() => handleMoodChange({ target: { value: '😐' } })}>😐</button>
-              <button type="button" className="text-3xl" onClick={() => handleMoodChange({ target: { value: '😔' } })}>😔</button>
+              <button type="button" className="text-5xl" onClick={() => handleMoodChange({ target: { value: '😊' } })}>😊</button>
+              <button type="button" className="text-5xl" onClick={() => handleMoodChange({ target: { value: '😐' } })}>😐</button>
+              <button type="button" className="text-5xl" onClick={() => handleMoodChange({ target: { value: '😔' } })}>😔</button>
               
               </div>
               {error && <p className="text-red-500 text-sm mt-1">{error}</p>} {/* Mostrar mensaje de error si existe */}
