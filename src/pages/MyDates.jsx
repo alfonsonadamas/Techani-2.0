11c7f83924a4407c35b3df475a37d81bdb2961e7
Vincitorecito <l20200338@morelia.tecnm.mx>
2024-03-20 21:44:11 -0600

    inicio de conexion mydates

--- src/pages/MyDates.jsx
+++ src/pages/MyDates.jsx
@@ -1,15 +1,47 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { createClient } from '@supabase/supabase-js';
 import SideBar from '../components/SideBar';
-function MyDates({ citas }) {
+
+function MyDates() {
+  const [citas, setCitas] = useState([]);
+
+  useEffect(() => {
+    async function obtenerCitas() {
+      // Configura el cliente Supabase
+      const supabaseUrl = 'https://techani.supabase.co';
+      const supabaseKey = 'TU_CLAVE_API_DE_SUPABASE';
+      const supabase = createClient(supabaseUrl, supabaseKey);
+
+      try {
+        // Realiza una consulta a la tabla de citas
+        const { data, error } = await supabase
+          .from('citas')
+          .select('*'); // Puedes seleccionar los campos que desees aquí
+        
+        // Maneja los errores
+        if (error) {
+          console.error('Error al obtener citas:', error.message);
+          return;
+        }
+        
+        // Establece las citas obtenidas en el estado
+        setCitas(data || []);
+      } catch (error) {
+        console.error('Error al obtener citas:', error.message);
+      }
+    }
+
+    // Llama a la función para obtener citas cuando el componente se monta
+    obtenerCitas();
+  }, []); // El segundo argumento de useEffect, [] en este caso, asegura que la función se ejecute solo una vez al montar el componente
+
   return (
     <div>
-        {console.log(citas)}
-        <SideBar />
+      <SideBar />
       <h2>Mis Citas</h2>
       <ul>
         {Array.isArray(citas) && citas.map((cita, index) => (
           <li key={index}>
-            
             <p>Fecha: {cita.date}</p>
             <p>Hora: {cita.time}</p>
             <p>Tipo de cita: {cita.appointmentType}</p>
@@ -18,8 +50,8 @@ function MyDates({ citas }) {
         ))}
       </ul>
     </div>
-    	
   );
 }
 
 export default MyDates;
+
