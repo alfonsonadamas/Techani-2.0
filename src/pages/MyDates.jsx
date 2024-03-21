8a12a09a452a5b2ebb0c32644f389b65eecd114e
Vincitorecito <l20200338@morelia.tecnm.mx>
2024-03-20 22:36:33 -0600

    ya se pueden ver mis citas

--- src/pages/MyDates.jsx
+++ src/pages/MyDates.jsx
@@ -1,57 +1,75 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { createClient } from '@supabase/supabase-js';
-import SideBar from '../components/SideBar';
+import { useUserContext } from "../context/UserContext";
+import { supabase } from '../config/supabase'; 
+import SideBar from "../components/SideBar";
 
 function MyDates() {
   const [citas, setCitas] = useState([]);
 
+
+  // Función para manejar la eliminación de una cita
+  const handleEliminarCita = (id) => {
+    // Lógica para eliminar la cita con el ID proporcionado
+    console.log("Eliminar cita con ID:", id);
+  };
+
+  // Función para manejar la edición de una cita
+  const handleEditarCita = (cita) => {
+    // Lógica para editar la cita proporcionada
+    console.log("Editar cita:", cita);
+  };
+
+  
   useEffect(() => {
     async function obtenerCitas() {
-      // Configura el cliente Supabase
-      const supabaseUrl = 'https://techani.supabase.co';
-      const supabaseKey = 'TU_CLAVE_API_DE_SUPABASE';
-      const supabase = createClient(supabaseUrl, supabaseKey);
-
       try {
-        // Realiza una consulta a la tabla de citas
         const { data, error } = await supabase
-          .from('citas')
-          .select('*'); // Puedes seleccionar los campos que desees aquí
+          .from('citasMedicas')
+          .select('*');
         
-        // Maneja los errores
         if (error) {
           console.error('Error al obtener citas:', error.message);
           return;
         }
         
-        // Establece las citas obtenidas en el estado
-        setCitas(data || []);
+        setCitas(data);
       } catch (error) {
         console.error('Error al obtener citas:', error.message);
       }
     }
 
-    // Llama a la función para obtener citas cuando el componente se monta
     obtenerCitas();
-  }, []); // El segundo argumento de useEffect, [] en este caso, asegura que la función se ejecute solo una vez al montar el componente
+  }, []);
 
   return (
     <div>
       <SideBar />
-      <h2>Mis Citas</h2>
-      <ul>
-        {Array.isArray(citas) && citas.map((cita, index) => (
-          <li key={index}>
-            <p>Fecha: {cita.date}</p>
-            <p>Hora: {cita.time}</p>
-            <p>Tipo de cita: {cita.appointmentType}</p>
-            <p>Lugar: {cita.location}</p>
-          </li>
-        ))}
-      </ul>
+      <div className="p-16 pt-20 sm:ml-64" data-aos="fade-up">
+        <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+          Mis citas
+        </label>
+        <ul className="mt-4">
+          {Array.isArray(citas) && citas.map((cita, index) => (
+            <li key={index} className="mb-4">
+              <div className="border border-gray-300 p-4 rounded-md">
+                <p>Fecha: {cita.date}</p>
+                <p>Hora: {cita.time}</p>
+                <p>Tipo de cita: {cita.appointmentType}</p>
+                <p>Lugar: {cita.location}</p>
+                <p>Nombre del Doctor: {cita.doctorName}</p>
+                <div className="mt-4 flex justify-end">
+                  <button className="mr-2 bg-red-500 hover:bg-red-600 text-white py-2 px-4 rounded-md" onClick={() => handleEliminarCita(cita.id)}>Eliminar</button>
+                  <button className="bg-blue-500 hover:bg-blue-600 text-white py-2 px-4 rounded-md" onClick={() => handleEditarCita(cita)}>Editar</button>
+                </div>
+              </div>
+            </li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
-}
+  
+}  
 
 export default MyDates;
-
