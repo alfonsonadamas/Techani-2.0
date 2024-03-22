107c4aebad591ceb0d50635a5e04bc523ba4181b
alfonsonadamas <gallardoalfonso01@gmail.com>
2024-03-21 21:01:04 -0600

    Merge con citas

--- src/pages/MyDates.jsx
+++ src/pages/MyDates.jsx
@@ -1,12 +1,11 @@
-import React, { useState, useEffect } from 'react';
-import { createClient } from '@supabase/supabase-js';
+import React, { useState, useEffect } from "react";
+import { createClient } from "@supabase/supabase-js";
 import { useUserContext } from "../context/UserContext";
-import { supabase } from '../config/supabase'; 
+import { supabase } from "../config/supabase";
 import SideBar from "../components/SideBar";
 
 function MyDates() {
   const [citas, setCitas] = useState([]);
-
 
   // Función para manejar la eliminación de una cita
   const handleEliminarCita = (id) => {
@@ -20,22 +19,19 @@ function MyDates() {
     console.log("Editar cita:", cita);
   };
 
-  
   useEffect(() => {
     async function obtenerCitas() {
       try {
-        const { data, error } = await supabase
-          .from('citasMedicas')
-          .select('*');
-        
+        const { data, error } = await supabase.from("citasMedicas").select("*");
+
         if (error) {
-          console.error('Error al obtener citas:', error.message);
+          console.error("Error al obtener citas:", error.message);
           return;
         }
-        
+
         setCitas(data);
       } catch (error) {
-        console.error('Error al obtener citas:', error.message);
+        console.error("Error al obtener citas:", error.message);
       }
     }
 
@@ -46,30 +42,40 @@ function MyDates() {
     <div>
       <SideBar />
       <div className="p-16 pt-20 sm:ml-64" data-aos="fade-up">
-        <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+        <label className="block text-gray-900 dark:text-white text-2xl mb-5 font-semibold">
           Mis citas
         </label>
         <ul className="mt-4">
-          {Array.isArray(citas) && citas.map((cita, index) => (
-            <li key={index} className="mb-4">
-              <div className="border border-gray-300 p-4 rounded-md">
-                <p>Fecha: {cita.date}</p>
-                <p>Hora: {cita.time}</p>
-                <p>Tipo de cita: {cita.appointmentType}</p>
-                <p>Lugar: {cita.location}</p>
-                <p>Nombre del Doctor: {cita.doctorName}</p>
-                <div className="mt-4 flex justify-end">
-                  <button className="mr-2 bg-red-500 hover:bg-red-600 text-white py-2 px-4 rounded-md" onClick={() => handleEliminarCita(cita.id)}>Eliminar</button>
-                  <button className="bg-blue-500 hover:bg-blue-600 text-white py-2 px-4 rounded-md" onClick={() => handleEditarCita(cita)}>Editar</button>
+          {Array.isArray(citas) &&
+            citas.map((cita, index) => (
+              <li key={index} className="mb-4">
+                <div className="border border-gray-300 p-4 rounded-md">
+                  <p>Fecha: {cita.date}</p>
+                  <p>Hora: {cita.time}</p>
+                  <p>Tipo de cita: {cita.typecites}</p>
+                  <p>Lugar: {cita.place}</p>
+                  <p>Nombre del Doctor: {cita.doctorName}</p>
+                  <div className="mt-4 flex justify-end">
+                    <button
+                      className="mr-2 bg-red-500 hover:bg-red-600 text-white py-2 px-4 rounded-md"
+                      onClick={() => handleEliminarCita(cita.id)}
+                    >
+                      Eliminar
+                    </button>
+                    <button
+                      className="bg-blue-500 hover:bg-blue-600 text-white py-2 px-4 rounded-md"
+                      onClick={() => handleEditarCita(cita)}
+                    >
+                      Editar
+                    </button>
+                  </div>
                 </div>
-              </div>
-            </li>
-          ))}
+              </li>
+            ))}
         </ul>
       </div>
     </div>
   );
-  
-}  
+}
 
 export default MyDates;
