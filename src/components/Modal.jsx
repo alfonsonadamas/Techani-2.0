7c33cff230b667e7e1f71ff8c766b5f8efea4cb6
FJUM <ferjum200@gmail.com>
2024-05-08 21:33:43 -0600

    Registro de Comidas

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -29,24 +29,4 @@ const Modal = ({ isOpen, onClose, children,title }) => {
     );
   };
 
-  export default Modal;import React from "react";
-
-export default function Modal({ open, children, onClose }) {
-  return (
-    <div
-      onClick={onClose}
-      className={`fixed inset-0 w-full h-screen flex justify-center items-center transition-colors ${
-        open ? "visible bg-black/20 overflow-y-auto" : "invisible"
-      }`}
-    >
-      <div
-        onClick={(e) => e.stopPropagation()}
-        className={`bg-white rounded-xl shadow p-6 transition-all ${
-          open ? "scale-100 opacity-100" : "scale-125 opacity-0"
-        }`}
-      >
-        {children}
-      </div>
-    </div>
-  );
-}
+  export default Modal;
