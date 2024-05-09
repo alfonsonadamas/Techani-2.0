1ba3cc02dc9b358dbe5e649b9043e7144ccde31f
FJUM <ferjum200@gmail.com>
2024-05-08 21:48:46 -0600

    Error de datos sin mostrar corregidos

--- src/pages/Alimentos.jsx
+++ src/pages/Alimentos.jsx
@@ -169,7 +169,7 @@ export default function Alimentos() {
     fetchTipoAlimento();
     getMealTypes();
     getRecords();
-  }, [alimentsSelect,carbohydratetotal]);
+  }, [user,alimentsSelect,carbohydratetotal]);
 
   return (
     <div>
--- src/pages/AllAlimentosRegister.jsx
+++ src/pages/AllAlimentosRegister.jsx
@@ -218,7 +218,7 @@ export default function AllAlimentosRegister () {
       getFilterfoodTyple();
       fetchTipoAlimento();
       fetchMedidas();
-    }, []);
+    }, [user]);
     
     return(
         <div>
