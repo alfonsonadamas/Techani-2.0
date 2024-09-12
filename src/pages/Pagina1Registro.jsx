bf71195f7038ff7b40ba04333e8b49c76e2dce9f
alfonsonadamas <gallardoalfonso01@gmail.com>
2024-09-11 18:23:52 -0600

    Primeros pasos con el expediente de la página 1

---
+++ src/pages/Pagina1Registro.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import SideBar from "../components/SideBar";
+import NavRegsitro from "../components/NavRegistro";
+
+export default function Pagina1Registro() {
+  return (
+    <div>
+      <NavRegsitro />
+    </div>
+  );
+}
