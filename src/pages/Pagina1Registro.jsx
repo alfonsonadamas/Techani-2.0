0ac1db79e43596dec909a41dd692291c9ce10223
glowar23 <ogl03140@gmail.com>
2024-09-24 16:31:25 -0600

    Formulario desplazable page 2

--- src/pages/Pagina1Registro.jsx
+++ src/pages/Pagina1Registro.jsx
@@ -104,8 +104,7 @@ export default function Pagina1Registro() {
   const Form2 = ({ onNext, onPrev }) => (
     <Formik
       initialValues={{
-        diabetesOption: "",
-        ageOption: "",
+
       }}
       onSubmit={(values) => {
         onNext()
@@ -119,39 +118,82 @@ export default function Pagina1Registro() {
         touched, }) => (
         <form onSubmit={handleSubmit}>
 
+
           <div className="bg-gray border-gray-700 w-fit shadow-lg flex flex-col items-center"
             style={{ backgroundColor: "#E7E7E7", width: "auto", height: "100vh" }}
           >
             <div
               className="bg-white border-radius rounded-xl  flex w-full h-full flex-col shadow-md"
-              style={{ maxWidth: "1080px", maxHeight: "800px", marginBlockEnd: "100px", marginBlockStart: "110px" }}
+              style={{ maxWidth: "90%", maxHeight: "80%", marginBlockStart: "110px" }}
             >
-            <div className="flex justify-end items-start h-10">
-                  <label htmlFor="pageNumber" className="self-end mt-4 mr-4" style={{ color: "#C3C3C3" }}>1 de 5</label>
-            </div>
-              <div className="flex flex-row justify-around"  >
+              <div className="flex justify-end items-start h-10">
+                <label htmlFor="pageNumber" className="self-end mt-4 mr-4" style={{ color: "#C3C3C3" }}>2 de 5</label>
+              </div>
+              <div className="grid grid-flow-col justify-stretch"  >
                 {/*LADO IZQUIERDO DEL FORMULARIO */}
-                <div htmlFor="left-size" className="justify-self-start">
+                <div htmlFor="left-size" className="justify-self-start pl-20 mr-4" style={{maxWidth: "800px", borderRight: '1px solid black' }}>
+                  
                   <InputType
                     input="text"
                     question="Nombre Completo"
                   />
+                  <InputType
+                    input="email"
+                    question="Correo Electrónico"
+                  />
+                  <InputType
+                    input="date"
+                    question="Fecha de Nacimiento"
+                  />
+                  <InputType
+                    input="select"
+                    question="Sexo Biologico"
+                    options={[{ option: "femenino", value: "Femenino" },
+                      { option: "masculino", value: "Masculino" }
+                      ]}
+                  />
+                  <InputType
+                    input="text"
+                    question="Lugar de Nacimiento"
+                  />
+
                   <button
                     className="self-start font-light place-self text-white mt-4 px-4 rounded-lg  hover:bg-blue-200 flex items-center justify-center"
                     style={{ backgroundColor: "#277BC0", border: "1px ridge #13BAFF" }} onClick={onPrev}
                   >
                     <b className="text-lg text-white pl-2">{"<"}</b>Anterior
                   </button>
+
                 </div>
 
-                <div htmlFor="right size" >
+                <div htmlFor="right size"
+                    className="flex flex-col justify-self-start pl-20"
+                >
                   <InputType
                     input="text"
-                    question="Nombre Completo"
+                    question="Estado de Nacimiento"
+                  />
+                  <InputType
+                    input="text"
+                    question="¿En que estado radica actualmente?"
+                  />
+                  <InputType
+                    input="date"
+                    question="Fecha en que fue diagnosticado"
+                  />
+  
+                  <InputType
+                    input="number"
+                    question="¿Que edad tenia cuando debutó?"
+                  />
+                  <InputType
+                    input="select"
+                    question="¿Estuvo hospitalizado cuando debutó?"
+                    options={[{option:"si", value:"Si"}, {option:"no", value:"No"}]}
                   />
                   <button
                     type="submit"
-                    className="font-light place-self text-white mt-4 px-4 rounded-lg  hover:bg-blue-200 flex items-center justify-center"
+                    className="self-end font-light place-self text-white mt-4 mr-4 rounded-lg  hover:bg-blue-200 flex items-center justify-center"
                     style={{ backgroundColor: "#277BC0", border: "1px ridge #13BAFF" }} onClick={onNext}
                   >
                     SIGUIENTE <b className="text-lg text-white pl-2">&gt;</b>
@@ -159,8 +201,8 @@ export default function Pagina1Registro() {
                 </div>
               </div>
               <div className="flex justify-between ">
-                
-                
+
+
               </div>
 
             </div>
